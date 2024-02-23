aa4f5a7b98c84a01b02fc9a900be99aa3b76742e
Amina <la_malek@esi.dz>
2024-02-23 22:57:28 +0100

    Cote Doctor

--- frontend/my-project/src/Components/OnSite.jsx
+++ frontend/my-project/src/Components/OnSite.jsx
@@ -13,16 +13,17 @@ function OnSite({patients}) {
     };
 
     return (
-        <div className="flex flex-col p-4 w-[80%]">
-            <span className="mb-4 text-x-[40px] text-blue font-bold"> Onsite Sessions </span>
-            <div className="py-2 bg-gray bg-opacity-60 rounded-xl">
+        <div className="w-[80%] p-4">
+            <span className="mb-6 text-x-[40px] text-blue font-bold"> Onsite Sessions </span>
+            <div className="py-2 mt-4 bg-gray bg-opacity-60 rounded-xl">
                 <div className="flex flex-row items-center justify-between py-2 space-x-10 text-x[32px] text-blue font-bold px-16">
                     <span>Name </span>
                     <span>Date</span>
                     <span>Starting hour</span>
                     <span>Ending hour</span>
                 </div>
-                    {generatePatients()}
+                {generatePatients()}
+                    
             </div>
    
         </div>
---
+++ frontend/my-project/src/Pages/Doctor2.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import NavDoctor from "../Components/NavDoctor";
+
+
+function Doctor2() {
+
+    const [date, setDate] = useState(null);
+
+    const onChange = date => {
+      setDate(date);
+      onDateSelect(date); 
+  
+    };
+
+    return(
+        <div className="BackDoctor2">
+            <NavDoctor/>
+            <div className="flex flex-col items-center p-20">
+                
+                <div className=" w-[40%]">
+                    <span className="text-2xl font-bold text-blue"> Selected Date </span>
+
+                    <div className="bg-LightGreen w-[100%] rounded p-4 mb-8 mt-4">
+                        <DatePicker
+                            selected={date}
+                            onChange={onChange}
+                            value={date}
+                            placeholderText="dd/mm/yyyy"
+                            showPopperArrow={false}
+                            dateFormat="dd/MM/yyyy" // Format de la date ici
+                            className="outline-none bg-LightGreen"
+                        />
+                    </div>        
+                </div>
+                
+            <div className="w-[40%]">
+                <span className="text-2xl font-bold text-blue"> Time Slots </span>    
+
+                <div className="flex flex-row p-4 bg-LightGreen w-[100%] items-center mt-4 cursor-pointer">
+                    <span className="text-blue text-opacity-30"> start date : end date </span>
+                </div>
+            </div>
+            
+            <div className="p-4 bg-LightGreen w-[40%] cursor-pointer mt-4 mb-8 text-center">
+                <span className="text-xl font-bold text-blue text-opacity-30"> Add this slot </span>
+            </div>
+            
+            <div className="p-4 bg-LightGreen w-[40%] items-center mt-8 cursor-pointer text-center">
+                <span className="text-2xl font-bold text-blue text-opacity-30 "> Save </span>
+            </div>
+
+
+        </div>
+   
+            
+        </div>
+        
+    );
+}
+
+export default Doctor2
